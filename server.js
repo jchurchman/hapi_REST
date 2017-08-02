cc855ba5124b370b5de0ce8b878418a4ac681498
Joe Churchman <churchman.joe@gmail.com>
2017-08-02 16:59:22 -0700

    got server to listen

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const restify = require('restify');
 
 const server = restify.createServer();
 
-server.connection({ port: 3000, host: 'localhost'});
-
-server.start((err) => {
+server.listen( 3000, (err) => {
 
     if(err) { throw err; }
 
-    console.log(`Server running at: ${server.info.uri}`);
+    console.log(`Server running at: ${server.url}`);
 });
 
 module.exports = server;
